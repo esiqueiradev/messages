fe0f850f137ca8ef5f70599e5d154f4bb4679d93
Eduardo Siqueira <esiqueira.dev@gmail.com>
2021-09-15 15:24:53 -0300

    Add Messages controller and validation

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+
+@Controller('/messages')
+export class MessagesController {
+  @Post()
+  create(@Body() body: CreateMessageDto) {
+    return body;
+  }
+
+  @Get()
+  getAll() {
+    return { message: 'get all' }
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') id: string) {
+    return { id }
+  }
+}
